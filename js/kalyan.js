6c9db71976689a21d7194d9ffc40d8981e7191a2
Евгений Колесников <kolei@yandex.ru>
2020-10-25 15:55:41 +0300

    11

--- js/kalyan.js
+++ js/kalyan.js
@@ -316,7 +316,7 @@ class ClassWatcher {
 $(document).ready(function ()
 {
     const moscowBound = [[55.142627, 36.803259],[56.021281, 37.967682]];
-    const version = 10;
+    const version = 11;
 
     var DEV_MODE = true;
     window.BRAND_CODE = '100000014';
