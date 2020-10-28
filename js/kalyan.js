1fe2bc01c4297f18bfe257f2a3af232ad8f5f656
Евгений Колесников <kolei@yandex.ru>
2020-10-28 07:54:52 +0300

    пока все смотрят на dev сервер

--- js/kalyan.js
+++ js/kalyan.js
@@ -1,4 +1,4 @@
-window.script_version = 21;
+window.script_version = 22;
 
 class UserData {
     props = {
@@ -291,7 +291,7 @@ $(document).ready(function ()
 
     var DEV_MODE = true;
     window.BRAND_CODE = '100000014';
-    if(window.location.hostname == 'kalyandostavka.ru'){
+    if(window.location.hostname == 'kalyandostavka-prod.ru'){
         window.CHAIHONA_HOST = 'https://chaihona1.ru';
         DEV_MODE = false;
     }
